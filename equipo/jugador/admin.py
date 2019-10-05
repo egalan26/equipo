3dde9d7cb273c480bce7fb44dfcd6e9e26bb140a
egalan26 <eduardo@z1.digital>
2019-10-05 22:24:10 +0200

    modelo persona

--- equipo/jugador/admin.py
+++ equipo/jugador/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Persona
+
+
+class PersonaAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "tfno", "email")
+    fields = (
+        "first_name",
+        "middle_name",
+        "last_name",
+        "tfno",
+        "email",
+    )
+    read_only_fields = ("created", )
+admin.site.register(Persona, PersonaAdmin)
